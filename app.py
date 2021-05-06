f8718b8b8d2593f4302f14fbef2cfd234098b5c2
timayyy <timilehin65@gmail.com>
2021-05-05 23:41:25 -0600

    prepare for deployment

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ cors = CORS(app, resources={r'/api/' : {"origins" : "", "headers" : "Content-Typ
 # Call the Universities function and store list in a variable 
 Universities = Universities()
 
-@app.errorhandler(404)
-def not_found(e):
-    return app.send_static_file('index.html')
+# @app.errorhandler(404)
+# def not_found(e):
+#     return app.send_static_file('index.html')
 
 @app.route("/")
 def serve():
